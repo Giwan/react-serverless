4016c12abbd575c2c3076c3a88ae382fce95a58a
Giwan Persaud <giwan.persaud@gmail.com>
2020-10-06 21:37:24 +0200

    updated login function

--- functions/login/login.js
+++ functions/login/login.js
@@ -1,15 +1,28 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+
+const authFailed = (message = "authentication failed") => ({
+    statusCode: 401,
+    body: JSON.stringify({ message }),
+});
+
 exports.handler = async (event, context) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+    try {
+        if (!event.body) return authFailed("username and password required");
+        const { username, password } = JSON.parse(event.body);
+
+        if (!(username && password)) {
+            return authFailed("invalid username password combination");
+        }
+
+        if (!(username === "admin" && password === "123")) {
+            return authFailed("invalid username password combination");
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "access granted" }),
+        };
+    } catch (err) {
+        return { statusCode: 500, body: err.toString() };
     }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
-  }
-}
+};

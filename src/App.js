6d54e8051b293bd7912261ec2585472b13745c8a
Giwan Persaud <giwan.persaud@gmail.com>
2020-10-06 20:22:17 +0200

    clean and editorConfig

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <button>Login</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Serverless functions demo</h1>
+            <button>Login</button>
+        </div>
+    );
 }
 
 export default App;
